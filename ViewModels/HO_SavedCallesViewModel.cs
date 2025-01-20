f32b3f1efa1566d5ebc16b581dc761c4b86a40b6
HansOr04 <hansalazar04@gmail.com>
2025-01-20 09:34:00 -0500

    creacion estructura de carpetas

---
+++ ViewModels/HO_SavedCallesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenHansOrtiz.ViewModels
+{
+    internal class HO_SavedCallesViewModel
+    {
+    }
+}
